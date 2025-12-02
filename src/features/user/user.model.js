c593befaa367da1b1c0452c0653fed19a55af803
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-12-02 21:58:14 +0530

    add class in userModel

---
+++ src/features/user/user.model.js
@@ -0,0 +1,10 @@
+
+export default class UserModel{
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    
+}
