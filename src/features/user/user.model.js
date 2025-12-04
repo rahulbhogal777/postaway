de218e0691c02ee5b20845527e9dfbc51bbeaa07
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-12-04 23:58:37 +0530

    signUP

--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -1,10 +1,20 @@
 
 export default class UserModel{
-    constructor(name, email, password) {
+    constructor(name, email, password, id) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.id = id;
+    }
+
+
+    static signUp(name, email, password) {
+        const user = new UserModel(name, email, password);
+        user.id = users.length() + 1;
+        users.push(user);
     }
 
     
 }
+
+let users = []
