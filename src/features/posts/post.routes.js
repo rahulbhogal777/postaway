bd53350942a1d447cb5df132059164ff087d57c4
rahulbhogal777 <rahulbhogal777@gmail.com>
2026-01-07 22:28:12 +0530

    update some files

--- src/features/posts/post.routes.js
+++ src/features/posts/post.routes.js
@@ -9,9 +9,9 @@ const postRoputer = express.Router();
 postRoputer.post('/', upload.single('imageUrl'), (req, res) => postController.createPost(req, res));
 postRoputer.get('/all', (req, res) => postController.getAllPosts(req, res));
 postRoputer.get('/', (req, res) => postController.getPostByUserId(req, res));
-postRoputer.get('/caption', (req, res) => postController.getPostByCaption(req, res));
 postRoputer.get('/:id', (req, res) => postController.getPostById(req, res));
 postRoputer.delete('/:id', (req, res) => postController.deletePostById(req, res));
 postRoputer.put('/:id', upload.single('imageUrl'), (req, res) => postController.updatePostById(req, res));
+postRoputer.get('/caption', (req, res) => postController.getPostByCaption(req, res));
 
 export default postRoputer;
